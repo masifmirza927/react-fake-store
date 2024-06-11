983f9514de5d4b60973b4dbdb2a3d99de8dbf271
Asif <masifmirza927@gmail.com>
2024-06-11 09:41:15 +0500

    header updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,11 +5,13 @@ import topImage from "../../images/top-bg.png"
 function Header () {
     return(
         <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 d-flex align-items-center">
-                       <h1 className="tagline">Glow up from the comfort of your home</h1>
-                    </div>
+            <div className="container h-100 d-flex flex-column justify-content-center align-items-start">
+                <div className="topBgWhite">top ideas of makup</div>
+                <h1 className="tagline">Glow up from the comfort <br />of your home</h1>
+                <p>Welcome to BeautyBox, your one-stop online destination for all things <br />beauty! Our virtual shelves are stocked with a vast array <br />of products from renowned brands.</p>
+                <div>
+                    <button className="btn btn-outline-danger">Shop now</button>
+                    <button type="button" className="btn btn-link">Follow instagram</button>
                 </div>
             </div>
         </>
